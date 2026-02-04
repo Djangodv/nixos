a76357ef1f856312d9f61daf4e8baa7987b2f8f9
Djangodv <dd.manders@protonmail.com>
2026-02-04 10:23:09 +0100

    refactor: removed global variables

--- nvim/lua/config/terminal.lua
+++ nvim/lua/config/terminal.lua
@@ -1,7 +1,7 @@
 -- Desc:
 
-vim.api.nvim_create_autocmd({'BufEnter'}, {
-	callback = function ()
+vim.api.nvim_create_autocmd({ 'BufEnter' }, {
+	callback = function()
 		if vim.bo.buftype == 'terminal' then
 			vim.cmd('startinsert')
 		end
@@ -10,23 +10,26 @@ vim.api.nvim_create_autocmd({'BufEnter'}, {
 
 vim.keymap.set('t', '<esc>', '<c-\\><c-n>')
 
-current_buf = nil
-current_win = nil
+local current = {
+	buf = nil,
+	win = nil
+}
+
 vim.api.nvim_create_user_command('Term', function()
-	if current_buf == nil then
+	if current.buf == nil then
 		vim.cmd('below split term://bash')
 		-- TODO: Resize window slightly
-		current_buf = vim.api.nvim_get_current_buf()
-		current_win = vim.api.nvim_get_current_win()
-		vim.api.nvim_set_option_value('buflisted', false, { buf = current_buf })
+		current.buf = vim.api.nvim_get_current_buf()
+		current.win = vim.api.nvim_get_current_win()
+		vim.api.nvim_set_option_value('buflisted', false, { buf = current.buf })
 	else
-		if vim.api.nvim_win_is_valid(current_win) then
-			vim.api.nvim_win_hide(current_win)
+		if vim.api.nvim_win_is_valid(current.win) then
+			vim.api.nvim_win_hide(current.win)
 		else
-			vim.api.nvim_open_win(current_buf, true, { split = 'below', win = 0, })
-			current_win = vim.api.nvim_get_current_win()
+			vim.api.nvim_open_win(current.buf, true, { split = 'below', win = 0, })
+			current.win = vim.api.nvim_get_current_win()
 		end
 	end
 end, {})
 
-vim.keymap.set({'t', 'n'}, '<c-j>', '<cmd>:Term<cr>')
+vim.keymap.set({ 't', 'n' }, '<c-j>', '<cmd>:Term<cr>')
