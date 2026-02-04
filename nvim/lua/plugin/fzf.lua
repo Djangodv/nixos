8ea8a56012cf9b9ec329a07ad7b5d8887ba21df6
Djangodv <dd.manders@protonmail.com>
2026-02-04 09:50:33 +0100

    fix: <esc> key not closing fzf

--- nvim/lua/plugin/fzf.lua
+++ nvim/lua/plugin/fzf.lua
@@ -1,30 +1,37 @@
 -- Desc:
 
-vim.pack.add({"https://github.com/junegunn/fzf.vim"})
+vim.pack.add({ "https://github.com/junegunn/fzf.vim" })
 
 -- vim.g.fzf_action = {
 --     ["ctrl-t"] = "tab split",
 --     ["ctrl-x"] = "split",
 --     ["ctrl-v"] = "vsplit",
---     ["ctrl-o"] = "",
 -- }
 
-vim.g.fzf_layout = { 
+vim.g.fzf_layout = {
 	["window"] = { ["width"] = 0.6, ["height"] = 0.6 }
 }
 
 -- Change default statusline colors when opening fzf
 local function fzf_statusline()
-  vim.opt_local.statusline = "%#fzf1# > %#fzf2#fz%#fzf3#f"
+	vim.opt_local.statusline = "%#fzf1# > %#fzf2#fz%#fzf3#f"
 end
 
 -- Create an autocommand to use above function
 vim.api.nvim_create_autocmd("User", {
-  pattern = "FzfStatusLine",
-  callback = fzf_statusline,
+	pattern = "FzfStatusLine",
+	callback = fzf_statusline,
 })
 
 -- Make preview window hidden by default
 vim.g.fzf_vim = {
-  preview_window = {'hidden,right,50%', 'ctrl-/'}
+	preview_window = { 'hidden,right,50%', 'ctrl-/' }
 }
+
+-- Needed to overwrite <esc> :Term specific keybind
+vim.api.nvim_create_autocmd('FileType', {
+	pattern = 'fzf',
+	callback = function()
+		vim.keymap.set('t', '<esc>', '<c-c>', { buffer = true })
+	end
+})
