64ebba72d271c60b818c95163278433695edc94c
Djangodv <dd.manders@protonmail.com>
2026-02-08 20:41:37 +0100

    feat: updated Vim options

--- nvim/lua/config/options.lua
+++ nvim/lua/config/options.lua
@@ -1,9 +1,42 @@
+-- Disable swapfiles
+vim.opt.swapfile = false
+
+-- Enable mouse
+vim.opt.mouse = 'a'
+
+-- Show linenumbers
+vim.opt.number = true
+
+-- Set colorscheme
 vim.cmd.colorscheme("kanagawa-dragon")
+
+-- Fix indentation (equal to LSP auto-format)
 vim.opt.tabstop = 2
 vim.opt.shiftwidth = 2
 
-vim.opt.swapfile = false
+-- Preview search and replace in a split window
+vim.opt.inccommand = 'split'
 
-vim.opt.mouse = 'a'
+-- Always keep a minimal amount of lines above and below cursor
+vim.opt.scrolloff = 10
 
-vim.opt.nu = true
+-- Ignore capital letters when searching
+vim.opt.ignorecase = true
+-- Automatically switch to case-sensitive search when typing an upppercase letter
+vim.opt.smartcase = true
+
+-- Show cursorline
+-- vim.opt.cursorline = true
+
+-- Disable line wrappign
+vim.opt.wrap = false
+-- Avoid wrapping lines in the middle of a word
+vim.opt.linebreak = true
+
+-- Disable auto-inserting comments
+vim.api.nvim_create_autocmd('BufEnter', {
+	command = 'set fo-=o | set fo-=r'
+})
+
+-- Persistent undo (file at: $XDG_STATE_HOME/nvim/undo/)
+vim.opt.undofile = true
