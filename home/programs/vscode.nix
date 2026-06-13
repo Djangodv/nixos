4ab0581faefbe3a0d1db47bb2927332ace41938f
Djangodv <dd.manders@protonmail.com>
2026-06-13 11:35:04 +0200

    feat: added VSCode configuration

---
+++ home/programs/vscode.nix
@@ -0,0 +1,131 @@
+{ pkgs, ... }:
+{
+
+	# Enable VSCode
+	programs.vscode = {
+		enable = true;
+	};
+
+	# Source: https://davi.sh/blog/2024/11/nix-vscode/
+	programs.vscode.profiles.default = {
+
+		# Extensions
+		extensions = with pkgs.vscode-extensions; [
+			vscodevim.vim
+			formulahendry.code-runner
+			yzane.markdown-pdf
+			vscodevim.vim
+			tomoki1207.pdf
+			ms-vscode.cpptools
+			ms-python.python
+			ritwickdey.liveserver
+		];
+
+		# Generate settings.json:
+    userSettings = {
+
+			# Focus on terminal after executing a file
+			"terminal.integrated.focusAfterRun" = "terminal";
+			# Use tab to cycle through list of completions
+			"editor.tabCompletion" = "on";
+			# Autosave files upon changing editor focus
+			"files.autoSave" = "afterDelay"; # Used to be "onFocusChange"
+			# Never ask for confirmation when performing certain actions
+			"security.workspace.trust.untrustedFiles" = "open";
+			"explorer.confirmDragAndDrop" = false;
+			"explorer.confirmDelete" = false;
+			# Show snippets as first item in autocomplete menu (enables snippet completion w. tab in *.md files)
+			"editor.snippetSuggestions" = "top";
+
+			# Visual preferences
+			"workbench.sideBar.location" = "right";
+			"window.commandCenter" = false; # Search bar at the top
+			"workbench.layoutControl.enabled" = false; # Top-right layout controls
+			# "chat.commandCenter.enabled" = false;
+			"editor.minimap.enabled" = false; # Code mini-map
+			"workbench.activityBar.location" = "top";
+			"window.menuBarVisibility" = "compact"; # Top-left menu
+
+			# Disable AI chat feature
+			"chat.disableAIFeatures" = true; 
+			# Hide secondary sidebar
+			"workbench.secondarySideBar.defaultVisibility" = "hidden";
+			# Disble annoying hint to start copilot in new empty file
+			"workbench.editor.empty.hint" = "hidden";
+
+			# Extension related settings
+			
+			# Exceptions for certain keysbindings to be handled by VSCode
+			"vim.handleKeys" = {
+					# <c-k> in VSCode is used for a bunch of keyboard shortcuts
+					"<C-k>" = false;
+					"<C-j>" = false;
+					"<C-p>" = false;
+					"<C-b>" = false;
+					"<C-h>" = false;
+					"<C-w>" = false;
+					"<C-d>" = false;
+			};
+
+			"vim.normalModeKeyBindings" = [
+					{
+							"before" = ["<space>"];
+							"after" = [":"];
+					}
+			];
+
+			"vim.visualModeKeyBindings" = [
+					{
+							"before" = ["<space>"];
+							"after" = [":"];
+					}
+			];
+
+			"code-runner.runInTerminal" = true;
+
+			# Deprecated
+			# Never insert a space after commenting code w. <c-/>
+			# "editor.comments.insertSpace" = false;
+			# Open new window horizontally = "down" (default value: "right")
+			# "workbench.editor.openSideBySideDirection" = "down";
+			# Use a vimrc file for specifying keybindings
+			# "vim.vimrc.enable" = true;
+
+		};
+
+		# Generate keybindings.json
+		keybindings = [
+			# Source: https://github.com/microsoft/vscode/issues/37366#issuecomment-566011854
+			{
+					"key" = "ctrl+tab";
+					"command" = "workbench.action.quickOpenPreviousRecentlyUsedEditor";
+					"when" = "!inEditorsPicker";
+			}
+			{
+					"key" = "ctrl+shift+v";
+					"command" = "markdown.showPreviewToSide";
+					"when" = "!notebookEditorFocused && editorLangId == 'markdown'";
+			}
+			{
+					"key" = "ctrl+j";
+					"command" = "workbench.action.quickOpenSelectNext";
+					"when" = "inQuickOpen";
+			}
+			{
+					"key" = "ctrl+k";
+					"command" = "workbench.action.quickOpenSelectPrevious";
+					"when" = "inQuickOpen";
+			}
+			{
+					"key" = "ctrl+l ctrl+f";
+					"command" = "search.action.openNewEditor";
+			}
+			{
+					"key" = "ctrl+l ctrl+c";
+					"command" = "workbench.files.action.collapseExplorerFolders";
+			}
+    ];
+		
+	};
+
+}
